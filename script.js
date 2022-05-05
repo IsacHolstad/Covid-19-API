b889cd35e267a62168a82a5ef3fa838c5793061a
IsacHolstad <wisac22@gmail.com>
2022-05-05 12:22:58 +0200

    showing a box to have data in

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ console.log(document.location);
 
 const myAPI = 'https://covid.mathdro.id/api'
 console.log(myAPI);
-const proxy = "https://noroffcors.herokuapp.com/";
-const corsFixUrl = proxy + myAPI;
+//const proxy = "https://noroffcors.herokuapp.com/";
+//const corsFixUrl = proxy + myAPI;
 
 const container = document.querySelector(".covid-container");
 
 async function results(){
     try{
-        const response = await fetch(corsFixUrl);
+        const response = await fetch(myAPI);
         console.log(response);
         const responseJSON = await response.json();
         console.log(responseJSON);
-        const covidData = responseJSON.results;
-        console.log(responseJSON);
-        for (let i = 0; i < responseJSON.length; i++) {
-            console.log(responseJSON[i].confirmed);
+        const covidData = responseJSON.data;
+        console.log(covidData);
+        for (let i = 0; i < covidData.length; i++) {
+            console.log(covidData[i].confirmed);
 
-            container.innerHTML += `<li>${responseJSON[i]}</li>`
+           // container.innerHTML += `<li>${responseJSON[i]}</li>`
         };
         
     }
