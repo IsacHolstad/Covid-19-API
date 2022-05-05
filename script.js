b8caa2736702973b7411b6fea1d677c226813088
IsacHolstad <wisac22@gmail.com>
2022-05-05 16:55:13 +0200

    getting new async await

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ console.log(myAPI)
 const resultContainer = document.querySelector(".container");
 
 
-
+async function cryptoNames() {
+    try{
+        const response = await fetch(myAPI);
+        console.log(response)
+        const responseJSON = await response.json(); // convert the response to json data
+        const cryptoData = responseJSON.data;
+        console.log( )
+        for (let i = 0; i < cryptoData.length; i++) {
+           
+            resultContainer.innerHTML += `<li>${cryptoData[i].Albania.All}</li>`;
+        }
+    }
+    catch(error) {
+        resultContainer.innerHTML += `<li>there is an error happening</li>`;
+       
+    }
+}
+cryptoNames();
 
 
