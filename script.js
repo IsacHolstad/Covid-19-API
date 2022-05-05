4035724d2275090f7f2d6ddc892fe4190e661701
IsacHolstad <wisac22@gmail.com>
2022-05-05 16:09:37 +0200

    making async await

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const myAPI = "https://covid19.mathdro.id/api"
+console.log(myAPI);
+
+const resultContainer = document.querySelector(".container");
+
+async function results(){
+    try{
+
+    }catch(error){
+        console.log(error)
+    }
+}
+results()
 
