1632eaec86becdf8f7c9c3e99ca6997abb8090bc
IsacHolstad <wisac22@gmail.com>
2022-05-05 16:48:53 +0200

    starting endpoint

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const myAPI = "https://covid-api.mmediagroup.fr/v1/cases"
-const proxy = "https://noroffcors.herokuapp.com/";
-const corsFixUrl = proxy + myAPI;
-console.log(corsFixUrl)
+
+console.log(myAPI)
 
 const resultContainer = document.querySelector(".container");
+
+
 
 
 
