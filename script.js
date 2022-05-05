fa921ecbeb1ea5675cd3637b99d196f70dba25ee
IsacHolstad <wisac22@gmail.com>
2022-05-05 12:17:38 +0200

    async await

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ console.log(document.location);
 
 const myAPI = 'https://covid.mathdro.id/api'
 console.log(myAPI);
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFixUrl = proxy + myAPI;
 
 const container = document.querySelector(".covid-container");
 
 async function results(){
     try{
-        const response = await fetch(myAPI);
+        const response = await fetch(corsFixUrl);
         console.log(response);
         const responseJSON = await response.json();
-        console.log(responseJSON)
+        console.log(responseJSON);
+        const covidData = responseJSON.results;
+        console.log(responseJSON);
+        for (let i = 0; i < responseJSON.length; i++) {
+            console.log(responseJSON[i].confirmed);
+
+            container.innerHTML += `<li>${responseJSON[i]}</li>`
+        };
         
     }
     catch(e){
