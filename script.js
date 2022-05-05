110a61be6c51cf3e3cf669c6368aff6c6ee72502
IsacHolstad <wisac22@gmail.com>
2022-05-05 12:08:25 +0200

    getting response for api

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const myAPI = 'https://covid.mathdro.id/api'
 console.log(myAPI);
 
 const container = document.querySelector(".covid-container");
+
+async function results(){
+    try{
+        const response = await fetch(myAPI);
+        console.log(response);
+        const responseJSON = await response.json();
+        console.log(responseJSON)
+        
+    }
+    catch(e){
+        console.log("errrrror")
+    }
+}
+results()
